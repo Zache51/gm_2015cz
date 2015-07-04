935b122168ce4a27ab35a677b25ae011cfb80a1e
Zacharias Hortén <zachetank@hotmail.com>
2015-07-04 23:41:43 +0200

    Started working on MeshObject

--- gmProject/gmProject/MeshObject.cpp
+++ gmProject/gmProject/MeshObject.cpp
@@ -1,11 +1,34 @@
 #include "MeshObject.hpp"
 
 
-MeshObject::MeshObject()
+
+/******************************* Private *******************************/
+
+
+
+bool MeshObject::loadObj(const char* filename, char* mtlFileName,
+	std::vector<Point>& points, std::vector<GLuint>& indices)
 {
+
 }
 
 
-MeshObject::~MeshObject()
+
+/***********************************************************************/
+/******************************* Public ********************************/
+
+
+
+MeshObject::MeshObject(const char* filename)
 {
+	char mtlFilename[] = "";
+	std::vector<Point> points;
+	std::vector<GLuint> indicies;
+
+	loadObj(filename, mtlFilename, points, indicies);
+}
+
+MtlContainer MeshObject::GetMtl() const
+{
+	return mtl;
 }
--- gmProject/gmProject/MeshObject.hpp
+++ gmProject/gmProject/MeshObject.hpp
@@ -1,8 +1,45 @@
 #pragma once
+
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <vector>
+//#include <stb_image.h>
+
+#define MESH_FOLDER "Meshes/"
+
+struct MtlContainer
+{
+	GLfloat Ns; // specular exponent
+	glm::vec3 Ka;
+	glm::vec3 Kd;
+	glm::vec3 Ks;
+	char* filename;
+};
+
 class MeshObject
 {
+private:
+
+	struct Point
+	{
+		glm::vec3 ver;
+		glm::vec3 vns;
+	};
+
+	MtlContainer mtl;
+
+	bool loadObj(const char* filename, char* mtlFileName, 
+	std::vector<Point>& points, std::vector<GLuint>& indices);
+	
+	
+	bool loadMtl(const char* filename, MtlContainer& mtl);
+	bool loadTexture(const char* filename);
+
 public:
-	MeshObject();
-	~MeshObject();
+
+	MeshObject(const char* filename);
+	~MeshObject(){}
+
+	MtlContainer GetMtl() const;
 };
 
